0613035edefaf75aadffff29a061f5dc1049fe6a
Connor Moody <connor@alphero.com>
2022-11-28 11:25:35 +1300

    Rewrite of step to assume that the version information is no longer in plists

--- step.sh
+++ step.sh
@@ -6,60 +6,37 @@
 set -e
 
 # ---------------------
-# --- Required Input
-
-if [ -z "${info_plist_path}" ]; then
-	echo "[ERROR] Required input is missing: please provide correct path to Info.plist file"
-	exit 1
-fi
-
-# ---------------------
 # --- Main
 
-ORIGIN_PATH=$(pwd)
-
-DIRECTORY=$(dirname "${info_plist_path}")
-FILENAME=$(basename "${info_plist_path}")
-
-echo "|"
-echo "[INFO] Switching working directory to [${DIRECTORY}]"
-cd "${DIRECTORY}"
-
-if [ ! -f "${FILENAME}" ]; then
-	echo "[ERROR] File is not found at given path: [${info_plist_path}]"
-	exit 1
-fi
-
-echo "[INFO] Reading data from [${info_plist_path}]..."
-
-version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${FILENAME}")
-build=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${FILENAME}")
-
-# Xcode 11+
-if [ $version == '$(MARKETING_VERSION)' ] || [ $build == '$(CURRENT_PROJECT_VERSION)' ]; then
-	echo "[INFO] Xcode 11+ project detected, reading version & build number from xcodeproj instead..."
-	cd "${ORIGIN_PATH}"
-	if [ -z "${xcodeproj_path}" ]; then
-		echo "[ERROR] Xcode project is not found at given path: [${xcodeproj_path}]"
-		exit 1
-	fi
+if [ ! -z "${xcodeproj_path}" ]; then # use the xcode project path if present
+	PROJECT_SPECIFIER="-project ${xcodeproj_path}"
 	if [ -z "${target}" ]; then
 		TARGET_SPECIFIER=""
 	else
-		TARGET_SPECIFIER="-target '${target}'"
+		TARGET_SPECIFIER="-target ${target}"
 	fi
-	echo "[INFO] Reading data from [${xcodeproj_path}]..."
-	PROJECT_SETTINGS=$(/bin/sh -c "xcodebuild -project ${xcodeproj_path} $TARGET_SPECIFIER -showBuildSettings")
-	if [ $version == '$(MARKETING_VERSION)' ]; then
-		version=$( echo "$PROJECT_SETTINGS" | grep "MARKETING_VERSION" | sed 's/[ ]*MARKETING_VERSION = //')
+elif [ ! -z "${xcworkspace_path}" ]; then # otherwise try and use the path to the xcode workspace
+	if [ ! -z "${scheme_name}" ]; then
+		WORKSPACE_SPECIFIER="-workspace ${xcworkspace_path} -scheme ${scheme_name}"
+	else
+		echo "[ERROR] Attempting to use path to a workspace but no scheme name was provided."
+		echo "$(xcrun xcodebuild -workspace ${xcworkspace_path} -list)"
+		exit 1
 	fi
-	if [ $build == '$(CURRENT_PROJECT_VERSION)' ]; then
-		build=$( echo "$PROJECT_SETTINGS" | grep "CURRENT_PROJECT_VERSION" | sed 's/[ ]*CURRENT_PROJECT_VERSION = //')
-	fi
-	echo "[INFO] Finished extracting data from [${xcodeproj_path}]."
 else
-	echo "[INFO] Finished extracting data from [${info_plist_path}]."
+	echo "[ERROR] No values given for xcodeproject_path or xcworkspace_path"
+	echo "[ERROR] A 'Project path' or 'Workspace path' MUST be provided"
+	exit 1
 fi
+
+
+PROJECT_SETTINGS=$(xcrun xcodebuild $PROJECT_SPECIFIER $TARGET_SPECIFIER $WORKSPACE_SPECIFIER -showBuildSettings)
+
+version=$( echo "$PROJECT_SETTINGS" | grep "MARKETING_VERSION" | sed 's/[ ]*MARKETING_VERSION = //')
+build=$( echo "$PROJECT_SETTINGS" | grep "CURRENT_PROJECT_VERSION" | sed 's/[ ]*CURRENT_PROJECT_VERSION = //')
+resolved_target=$( echo "$PROJECT_SETTINGS" | grep "TARGET_NAME" | sed 's/[ ]*TARGET_NAME = //')
+
+echo "[INFO] Resolved to target: ${resolved_target}"
 
 envman add --key XPI_VERSION --value "${version}"
 echo "[INFO] Version: ${version} -> Saved to \$XPI_VERSION environment variable."
